bdf06a9648bbc8ecea6aff22434b6773a97b1168
elviajero971 <lucas.illiano@hotmail.com>
2021-03-17 09:51:08 +0100

    update path

--- src/App.js
+++ src/App.js
@@ -70,8 +70,7 @@ function App() {
             </Route>
             <Route path="/quotes">
               <Quote/>
-            </Route>
-            
+            </Route> 
           </Switch>
         </div>
       </div>
